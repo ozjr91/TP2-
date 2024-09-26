6bd4aa7eb0ebe64dd5a6e194d1162465cd4e5e8c
ozjr91 <95700531+ozjr91@users.noreply.github.com>
2024-09-26 13:47:21 +0200

    Update fonctions.c

--- TP2/fonctions.c
+++ TP2/fonctions.c
@@ -1,14 +1,13 @@
 #include <assert.h>
 #include "fonctions.h"
 
-/* Pré-condition : on suppose n >= 0*/
+
 long int factorielle(int n) {
     if(n<=1) 
         return 1;
     return n*factorielle(n-1);
 }
 
-/* Pré-condition : on suppose n > 0, m >= 0 et n >= m */
 long int coefficient(int n, int m) {
     return factorielle(n) / (factorielle(m) * factorielle(n-m));
 }
